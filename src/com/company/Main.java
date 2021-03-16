1d50693e22f1328784de9dee27a08cc501bc75da
ignashagood <ignatik2186@gmail.com>
2021-03-17 00:34:02 +0300

    Создал метод заполнения массивов

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -8,28 +9,57 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         String path = "C:\\Users\\ignat\\Desktop\\arrays.txt";
         File file = new File(path);
+        if (!file.exists()) {
+            System.out.println("Файл не найден. Программа завершает работу с ошибкой.");
+            return;
+        }
         Scanner scanner = new Scanner(file);
+        int a, b;
 
-        int[] array1 = new int[Integer.parseInt(scanner.nextLine())];
+        try {
+            a = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException exception) {
+
+            System.out.println("Первая строка в файле не содержит число. Программа завершает работу с ошибкой.");
+            return;
+        }
+
         String line = scanner.nextLine();
-        String[] numbers = line.split(" ");
-        int i, counter = 0;
-        for(String number : numbers) {
-            array1[counter++] = Integer.parseInt(number);
-        }
-        counter = 0;
-        System.out.print("Первый массив: ");
-        for(i = 0; i < array1.length; i++)
-            System.out.print(array1[i] + " ");
+        int[] array1 = convertStringToArray(a, line);
 
-        int[] array2 = new int[Integer.parseInt(scanner.nextLine())];
-        line = scanner.nextLine();
-        numbers = line.split(" ");
-        for(String number : numbers) {
-            array2[counter++] = Integer.parseInt(number);
+        System.out.print("Первый массив: ");
+        for (int j : array1) System.out.print(j + " ");
+
+        try {
+            b = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException exception) {
+            System.out.println("Третья строка в файле не содержит число. Программа завершает работу с ошибкой.");
+            return;
         }
+        line = scanner.nextLine();
+        int[] array2 = convertStringToArray(b, line);
         System.out.print("Второй массив: ");
-        for(i = 0; i < array2.length; i++)
-            System.out.print(array2[i] + " ");
+        for (int j : array2) System.out.print(j + " ");
+    }
+
+    private static int[] convertStringToArray(int length, String line) {
+        String[] numbers = line.split(" ");
+        int[] array = new int[length];
+        int counter = 0;
+        outer:
+        for (String number : numbers) {
+            if (counter == length) break;
+            int checker = Integer.parseInt(number);
+            for (int i = 0; i < counter; i++) {
+                if (array[i] == checker) continue outer;
+            }
+            array[counter++] = checker;
+        }
+        if (counter < length) {
+            int[] finalArray = new int[counter];
+            System.arraycopy(array, 0, finalArray, 0, counter);
+            return finalArray;
+        }
+        return array;
     }
 }
